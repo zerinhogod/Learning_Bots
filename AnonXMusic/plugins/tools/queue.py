518b8c05bd08097d6c9d3b89929b781d50ac7415
zerinhogod <luceenavictor@gmail.com>
2024-08-10 22:29:10 -0300

    Update queue.py

--- AnonXMusic/plugins/tools/queue.py
+++ AnonXMusic/plugins/tools/queue.py
@@ -165,11 +165,11 @@ async def queued_tracks(client, CallbackQuery: CallbackQuery, _):
     for x in got:
         j += 1
         if j == 1:
-            msg += f'Streaming :\n\n✨ Title : {x["title"]}\nDuration : {x["dur"]}\nBy : {x["by"]}\n\n'
+            msg += f'Reproduzindo:\n\n✨ Título: {x["title"]}\nDuração: {x["dur"]}\nPor: {x["by"]}\n\n'
         elif j == 2:
-            msg += f'Queued :\n\n✨ Title : {x["title"]}\nDuration : {x["dur"]}\nBy : {x["by"]}\n\n'
+            msg += f'Na fila:\n\n✨ Título: {x["title"]}\nDuração: {x["dur"]}\nPor: {x["by"]}\n\n'
         else:
-            msg += f'✨ Title : {x["title"]}\nDuration : {x["dur"]}\nBy : {x["by"]}\n\n'
+            msg += f'✨ Título: {x["title"]}\nDuração: {x["dur"]}\nPor: {x["by"]}\n\n'
     if "Queued" in msg:
         if len(msg) < 700:
             await asyncio.sleep(1)
