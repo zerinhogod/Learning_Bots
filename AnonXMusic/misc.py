ff851c318f83439eb4b85113e8aa7ae55c21678c
zerinhogod <luceenavictor@gmail.com>
2024-08-10 23:01:10 -0300

    Update misc.py

--- AnonXMusic/misc.py
+++ AnonXMusic/misc.py
@@ -39,7 +39,7 @@ XCB = [
 def dbb():
     global db
     db = {}
-    LOGGER(__name__).info(f"Local Database Initialized.")
+    LOGGER(__name__).info(f"Banco de dados local inicializado.")
 
 
 async def sudo():
@@ -58,7 +58,7 @@ async def sudo():
     if sudoers:
         for user_id in sudoers:
             SUDOERS.add(user_id)
-    LOGGER(__name__).info(f"Sudoers Loaded.")
+    LOGGER(__name__).info(f"Sudoers carregados.")
 
 
 def heroku():
@@ -68,8 +68,8 @@ def heroku():
             try:
                 Heroku = heroku3.from_key(config.HEROKU_API_KEY)
                 HAPP = Heroku.app(config.HEROKU_APP_NAME)
-                LOGGER(__name__).info(f"Heroku App Configured")
+                LOGGER(__name__).info(f"Aplicativo Heroku configurado")
             except BaseException:
                 LOGGER(__name__).warning(
-                    f"Please make sure your Heroku API Key and Your App name are configured correctly in the heroku."
+                    f"Certifique-se de que sua chave de API Heroku e o nome do seu aplicativo estejam configurados corretamente no Heroku."
                 )
